b3af3bb4be0a1f3e0945776873e22d92eb25981d
zakiego <belummikir@gmail.com>
2023-04-30 11:51:47 +0800

    feat: add push command

--- Dockerfile
+++ Dockerfile
@@ -17,32 +17,16 @@ RUN chmod +x /entrypoint-test.sh
 
 # Create a test folder and initialize a git repository
 
-# Test with arguments
-ARG TEST_GIT_USERNAME="John Doe"
-ARG TEST_GIT_EMAIL="johndoe@example.com"
-ARG TEST_GIT_COMMIT_MESSAGE="Initial commit"
-
-RUN mkdir test && \
-    cd test && \
-    git init && \
-    touch dummy.txt && \
-    ../../entrypoint-test.sh -u "${TEST_GIT_USERNAME}" -e "${TEST_GIT_EMAIL}" -m "${TEST_GIT_COMMIT_MESSAGE}" && \
-    git log -1 | grep "${TEST_GIT_COMMIT_MESSAGE}" && \
-    cd .. && \
-    rm -rf test
-
-# Test with environment variables
-
-ENV USERNAME="$TEST_GIT_USERNAME"
-ENV EMAIL="$TEST_GIT_EMAIL"
-ENV COMMIT_MESSAGE="$TEST_GIT_COMMIT_MESSAGE"
+ENV USERNAME="John Doe"
+ENV EMAIL="johndoe@example.com"
+ENV COMMIT_MESSAGE="Initial commit"
 
 RUN mkdir test && \
     cd test && \
     git init && \
     touch dummy.txt && \
     ../../entrypoint-test.sh && \
-    git log -1 | grep "${TEST_GIT_COMMIT_MESSAGE}" && \
+    git log -1 | grep "${COMMIT_MESSAGE}" && \
     cd .. && \
     rm -rf test
 
--- entrypoint-test.sh
+++ entrypoint-test.sh
@@ -2,42 +2,6 @@
 
 set -e
 
-# Default values for input parameters
-username=""
-email=""
-commit_message=""
-
-# Parse command line arguments
-while getopts "u:e:m:" opt; do
-  case ${opt} in
-    u )
-      username=$OPTARG
-      ;;
-    e )
-      email=$OPTARG
-      ;;
-    m )
-      commit_message=$OPTARG
-      ;;
-    \? )
-      echo "Invalid option: -$OPTARG" 1>&2
-      exit 1
-      ;;
-    : )
-      echo "Option -$OPTARG requires an argument." 1>&2
-      exit 1
-      ;;
-  esac
-done
-
-# If no command line arguments were provided, check for environment variables
-if [[ -z "$username" && -z "$email" ]]; then
-  username=${USERNAME}
-  email=${EMAIL}
-  commit_message=${COMMIT_MESSAGE}
-fi
-
-
 # Configure Git user and email
 git config --global user.email "$email"
 git config --global user.name "$username"
@@ -50,5 +14,5 @@ if [ -n "$(git status --porcelain)" ]; then
   git commit -m "$commit_message"
 
   # Push changes to the remote repository
-  # git push
+  # git push "$remote" "$branch" -u "$username" -p "$password"
 fi
